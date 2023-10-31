c306b0cebce27cc3404f592661f0ea4b279a8dec
Lucy Gao <lucystatsca@gmail.com>
2023-10-31 10:32:18 -0700

    adding documentation for square

--- R/square.R
+++ R/square.R
@@ -1,3 +1,12 @@
+#' Squares numbers
+#'
+#' @param x a numeric atomic, vector, or list
+#'
+#' @return The square of \code{x}
+#' @export
+#'
+#' @examples
+#' square(10)
 square <- function(x) {
   x^2
 }
