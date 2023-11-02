64a4857d865b9eed33cc9caf78200960cf16458a
Lucy Gao <lucystatsca@gmail.com>
2023-11-02 10:40:12 -0700

    Added dataset to package

---
+++ R/patients.R
@@ -0,0 +1,11 @@
+#' Patients data
+#'
+#' Made up data for illustration purposes.
+#'
+#' @format ## `patients`
+#' A data frame with 3 rows and 2 columns:
+#' \describe{
+#'   \item{id}{ID}
+#'   \item{age}{Age}
+#' }
+"patients"
